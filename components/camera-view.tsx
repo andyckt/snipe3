c20f283e45cc7de6e5debb85347a612c3cf60138
Andy <kamtocheung1104@gmail.com>
2025-09-16 14:14:20 +0800

    move the countdown to the centre and more upwards

--- components/camera-view.tsx
+++ components/camera-view.tsx
@@ -25,9 +25,11 @@ export function CameraView({ videoRef, countdown, recordingTimeLeft }: CameraVie
 
       {/* Recording Time Left */}
       {recordingTimeLeft !== null && (
-        <div className="absolute top-4 right-4 bg-black bg-opacity-50 rounded-lg px-3 py-1">
-          <div className="text-white text-xl font-semibold">
-            {Math.floor(recordingTimeLeft / 60)}:{(recordingTimeLeft % 60).toString().padStart(2, '0')}
+        <div className="absolute top-12 left-0 right-0 flex justify-center">
+          <div className="bg-black bg-opacity-50 rounded-lg px-4 py-2">
+            <div className="text-white text-2xl font-semibold">
+              {Math.floor(recordingTimeLeft / 60)}:{(recordingTimeLeft % 60).toString().padStart(2, '0')}
+            </div>
           </div>
         </div>
       )}
